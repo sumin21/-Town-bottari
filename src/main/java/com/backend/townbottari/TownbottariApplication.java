d497089f28642af8edbf1f136fc3d04d8af00dba
sumin21 <tnals1178@naver.com>
2023-05-08 17:02:22 +0900

    first commit

---
+++ src/main/java/com/backend/townbottari/TownbottariApplication.java
@@ -0,0 +1,13 @@
+package com.backend.townbottari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TownbottariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TownbottariApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/townbottari/TownbottariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.townbottari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TownbottariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
